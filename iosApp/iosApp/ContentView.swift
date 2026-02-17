89b356755ecac62bfb6788b5e31fa6257c3858dc
Jared Burrows <jaredsburrows@gmail.com>
2026-02-16 20:02:48 -0500

    more clean up

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,19 +3,16 @@ import SwiftUI
 import Shared
 
 struct ComposeView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.mainViewController()
-    }
-
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+  func makeUIViewController(context: Context) -> UIViewController {
+    MainViewControllerKt.mainViewController()
+  }
+  
+  func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
 struct ContentView: View {
-    var body: some View {
-        ComposeView()
-            .ignoresSafeArea()
-    }
+  var body: some View {
+    ComposeView()
+      .ignoresSafeArea()
+  }
 }
-
-
-
